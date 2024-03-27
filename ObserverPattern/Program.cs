1b37cadbd778df8c969b0c0282810df9c20777aa
Ahmed Montasser <1700205@eng.asu.edu.eg>
2024-03-27 18:25:18 +0200

    Observer Pattern Implementation

--- ObserverPattern/Program.cs
+++ ObserverPattern/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using SystemComponents.Observers;
+using SystemComponents.Subject;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+OfficialGas officialGas = new OfficialGas();
+
+TotalStation totalStation = new TotalStation(officialGas);
+
+totalStation.Subscribe();
+
+officialGas.setPrice(2.00);
+officialGas.setPrice(1.00);
+
+totalStation.UnSubscribe();
+officialGas.setPrice(1.00);
+officialGas.setPrice(2.00);
+
