0425e9c37e24e2efb7319b4ce866ee23cb91d7f1
Ahmed Montasser <1700205@eng.asu.edu.eg>
2024-04-01 17:55:53 +0200

    Senario

--- SingletonPattern/Program.cs
+++ SingletonPattern/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using SystemComponents;
+using SystemComponents.Providers;
+using SystemComponents.Singleton;
+
+MaleProvider m = new MaleProvider();
+
+StoreSingleton s1 = m.getStoreSingleton();
+
+Console.WriteLine("---------------------------------------------------");
+
+s1.PrintNames(); //Ahmed, Mohamed, Hamed
+Console.WriteLine("---------------------------------------------------");
+FemaleProvider f = new FemaleProvider();
+
+//StoreSingleton from MaleProvider
+
+ s1 = m.getStoreSingleton();
+
+
+
+s1.PrintNames(); //Ahmed, Mohamed, Hamed, Samah, Manal, Hend
+
+//StoreSingleton from FemaleProvider which should be same of MaleProvider
+
+Console.WriteLine("---------------------------------------------------");
+s1 = f.getStoreSingleton();
+
+s1.PrintNames(); //Ahmed, Mohamed, Hamed, Samah, Manal, Hend
+Console.WriteLine("---------------------------------------------------");
