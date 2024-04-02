f446968af940b2d203da4f747baf251d6d172be8
Ahmed Montasser <1700205@eng.asu.edu.eg>
2024-04-02 18:47:35 +0200

    Client

---
+++ CommandPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SystemComponents.ConcreteCommands;
+using SystemComponents.Interfaces;
+using SystemComponents.Invoker;
+using SystemComponents.Receivers;
+
+Console.WriteLine("Hello, World!");
+
+
+ApiInvoker apiInvoker = new ApiInvoker();
+
+Console.WriteLine(apiInvoker);
+
+apiInvoker.performFirstCommand().Wait();
+
+API azanApi = new AzanAPI();
+
+Command azanCommand = new APICommand(azanApi);
+
+apiInvoker.setFirstCommand(azanCommand);
+
+apiInvoker.performFirstCommand().Wait();
+
+Console.WriteLine(apiInvoker);
+
