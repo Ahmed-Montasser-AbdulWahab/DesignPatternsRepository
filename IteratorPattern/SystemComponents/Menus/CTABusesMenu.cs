c2c9013aa0aa1188294e90962e763cb38439114e
Ahmed Montasser <1700205@eng.asu.edu.eg>
2024-04-11 23:35:23 +0200

    Adding Collections

---
+++ IteratorPattern/SystemComponents/Menus/CTABusesMenu.cs
@@ -0,0 +1,56 @@
+﻿using IteratorPattern.SystemComponents.Iterators;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern.SystemComponents.Menus
+{
+    public class CTABusesMenu : Menu
+    {
+        private readonly CTABus[] ctaBuses;
+        private const int NUMBER_OF_BUSES = 10;
+        
+
+        public CTABusesMenu()
+        {
+            ctaBuses = new CTABus[NUMBER_OF_BUSES];
+            AddCTABus("1001", "First Settlement", "AbdulMoneim Riyadh");
+            AddCTABus("1002", "Masakn Ain Shams", "Matbaa");
+            AddCTABus("1003", "Cairo Airport", "Bolak Dakroor");
+            AddCTABus("1004", "Salam City", "Ataba");
+            
+
+
+
+        }
+
+        private void AddCTABus (string line, string terminalOne, string terminalTwo)
+        {
+            if (filled < NUMBER_OF_BUSES) { ctaBuses[filled++] = new CTABus(line, terminalOne, terminalTwo); }
+            else { throw new IndexOutOfRangeException("Menu is filled"); } 
+        }
+
+        public override IEnumerator<Bus> GetEnumerator()
+        {
+            return new CtaIterator(ctaBuses);
+        }
+
+
+        public int Filled { get { return filled; } }
+
+        public override string? this[int index] { get {
+                if(index < NUMBER_OF_BUSES)
+                {
+                    return ctaBuses[index]?.Description;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            } }
+
+    }
+}
---
+++ IteratorPattern/SystemComponents/Menus/CompanyBusesMenu.cs
@@ -0,0 +1,55 @@
+﻿using IteratorPattern.SystemComponents.Iterators;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern.SystemComponents.Menus
+{
+    public class CompanyBusesMenu : Menu
+    {
+        private readonly List<CompanyBus> _companyBuses;
+
+        public CompanyBusesMenu()
+        {
+            _companyBuses = new List<CompanyBus>();
+            AddCompanyBus("Q3", "Obour City", "AbdulMoneim Riyadh", "Engliziyah");
+            AddCompanyBus("M5", "Fifth Settlement", "AbdulMoneim Riyadh", "Mwasalat Misr");
+            AddCompanyBus("219", "Bashteel", "Salam BusStop", "Nozha");
+            AddCompanyBus("106", "Ataba", "Salam BusStop", "Sakr");
+
+            
+        }
+
+
+        private void AddCompanyBus(string line, string terminalOne, string terminalTwo, string company)
+        {
+            _companyBuses.Add(new CompanyBus(company, line, terminalOne, terminalTwo));
+        }
+
+        public override IEnumerator<Bus> GetEnumerator()
+        {
+            return new CompanyIterator(_companyBuses);
+        }
+
+
+
+        public override string? this[int index]
+        {
+            get
+            {
+                if (index < Filled)
+                {
+                    return _companyBuses[index]?.Description;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+    }
+}
