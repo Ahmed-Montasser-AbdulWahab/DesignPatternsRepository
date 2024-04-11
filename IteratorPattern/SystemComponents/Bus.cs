86d38cc695487869148a2e4ff29279ed22c399e6
Ahmed Montasser <1700205@eng.asu.edu.eg>
2024-04-11 23:35:04 +0200

    Adding Base Elements

---
+++ IteratorPattern/SystemComponents/Bus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern.SystemComponents
+{
+    public abstract class Bus
+    {
+        protected string _terminalOne = "N/A";
+        protected string _terminalTwo = "N/A";
+        protected string _line = "N/A";
+
+        public string TerminalOne { get { return _terminalOne; } set { _terminalOne = value; } }
+        public string TerminalTwo { get { return _terminalTwo; } set { _terminalTwo = value; } }
+
+        public string Line { get { return _line; } set { _line = value; } }
+
+
+        public string Description { get => $"{_line} : {_terminalOne} / {_terminalTwo}"; }
+
+    }
+}
